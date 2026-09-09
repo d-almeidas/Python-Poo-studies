2b02304193b5d991d28fcd8f07f88d13996f727d
d-almeidas <neaa01111@gmail.com>
2026-09-09 14:09:38 -0300

    adicionei .py em todos os exercicios

--- Poo-Exercices/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-    quercontinuar = input("Deseja continuar? (s/n): ")
-    while quercontinuar.lower() not in ['s', 'n']:
-        print("[red]Opção inválida! Digite 's' para sim ou 'n' para não.[/red]")
-        quercontinuar = input("Deseja continuar? (s/n): ")
-    if quercontinuar.lower() == 'n':
-        print("[blue]Saindo do sistema...[/blue]")
-        break 
